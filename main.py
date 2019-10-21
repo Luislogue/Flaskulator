d58e496858f82ed887537c8e05e9306619771461
Luislogue <lulogue97@gmail.com>
2019-10-21 10:28:50 +0200

    PERDER FUNCIONA

--- main.py
+++ main.py
@@ -18,11 +18,20 @@ def ruleta():
     bonuses = request.form
     fijos = ['1000€', 'Perder', 'mitad_dinero', '500€', '200€', '-turno-1000€']
     tirada = random.choice(fijos)
+    if tirada == 'Perder':
+      return render_template("perder.html", tirada=tirada)
+    
     with open('tiradas.txt', 'a+') as f:
       f.write(tirada + '\n')
       f.close()
-    return render_template("ruleta.html", bonuses=bonuses, fijos=fijos, fijo=tirada)
+    historial = open('tiradas.txt', 'r')
+    r_historial = historial.read()
+    
+  return render_template("ruleta.html", bonuses=bonuses, fijos=fijos, fijo=tirada, historial=r_historial)
 
+# def perder(tirada):
+#   if tirada == 'Perder':
+#     return render_template("perder.html", tirada=tirada)
 
 if __name__ == "__main__":
     app.run(debug=True)
